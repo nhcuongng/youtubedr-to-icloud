47abf698950fa6cb82083e688a1ad507d6a0b172
Cuong Nguyen Huu <cuongnh@fint.vn>
2026-01-30 00:28:13 +0700

    fix: update missing script

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const fs = require("fs-extra");
 const { getYoutubeMyMix } = require("./youtube-my-mix");
-var { DOWNLOAD_FOLDER } = require("./utils");
 
-(async () => {
-  await getYoutubeMyMix();
-})();
+getYoutubeMyMix();
